76dc1fa59796750444af9a659abbf095e657495f
ALLAN <allankipr6@gmail.com>
2024-07-01 14:40:55 +0300

    completed lab

--- server/models.py
+++ server/models.py
@@ -2,8 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
-
+delete = db.session.delete
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
-
+    delete = db.session.delete
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
+
+    def __repr__(self):
+        return f'<Plant {self.name} | Price: {self.price}>'
